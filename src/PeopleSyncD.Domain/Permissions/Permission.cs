16059af7775e23fd7e45b826a65b1fc94b9da023
JASON HENDERSON <deaconjason@medisyncdtechnologies.com>
2026-08-05 03:42:40 -0500

    fix: satisfy final M1 permission naming analyzer

--- src/PeopleSyncD.Domain/Permissions/Permission.cs
+++ src/PeopleSyncD.Domain/Permissions/Permission.cs
@@ -3,7 +3,7 @@ namespace PeopleSyncD.Domain.Permissions;
 /// <summary>
 /// Stable permission identifiers used by authorization policies.
 /// </summary>
-public static class Permission
+public static class PermissionNames
 {
     public const string OrganizationsRead = "organizations.read";
     public const string OrganizationsWrite = "organizations.write";
