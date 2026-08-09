5c4ca2a4114e9eacd694e349840c8279b3d715d3
JASON HENDERSON <deaconjason@medisyncdtechnologies.com>
2026-08-09 01:56:32 -0500

    feat: register tenant access service

--- src/PeopleSyncD.Infrastructure/DependencyInjection.cs
+++ src/PeopleSyncD.Infrastructure/DependencyInjection.cs
@@ -1,24 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PeopleSyncD.Infrastructure.Authorization;
 using PeopleSyncD.Infrastructure.Persistence;
 
 namespace PeopleSyncD.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(
-        this IServiceCollection services,
-        IConfiguration configuration)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("peoplesyncd");
-
         if (!string.IsNullOrWhiteSpace(connectionString))
-        {
-            services.AddDbContext<PeopleSyncDDbContext>(options =>
-                options.UseNpgsql(connectionString));
-        }
+            services.AddDbContext<PeopleSyncDDbContext>(options => options.UseNpgsql(connectionString));
 
+        services.AddScoped<TenantAccess>();
         return services;
     }
 }
