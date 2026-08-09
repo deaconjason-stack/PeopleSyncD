ef18fd425bd8bef8f6ed5aa09d675a43f1d46dcb
JASON HENDERSON <deaconjason@medisyncdtechnologies.com>
2026-08-09 01:50:57 -0500

    feat: register identity persistence models

--- src/PeopleSyncD.Infrastructure/Persistence/PeopleSyncDDbContext.cs
+++ src/PeopleSyncD.Infrastructure/Persistence/PeopleSyncDDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PeopleSyncD.Domain.Identity;
 using PeopleSyncD.Domain.Organizations;
 using PeopleSyncD.Domain.People;
 
@@ -8,6 +9,8 @@ public sealed class PeopleSyncDDbContext(DbContextOptions<PeopleSyncDDbContext> 
 {
     public DbSet<Organization> Organizations => Set<Organization>();
     public DbSet<Person> People => Set<Person>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<OrganizationMembership> OrganizationMemberships => Set<OrganizationMembership>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
